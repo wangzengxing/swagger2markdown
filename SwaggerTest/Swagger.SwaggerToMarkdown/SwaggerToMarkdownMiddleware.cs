360356f60603d27be3b8bc8013a40e0788f20c48
boyue <1625322170@qq.com>
2021-01-21 14:33:44 +0800

    返回指定文档

--- SwaggerTest/Swagger.SwaggerToMarkdown/SwaggerToMarkdownMiddleware.cs
+++ SwaggerTest/Swagger.SwaggerToMarkdown/SwaggerToMarkdownMiddleware.cs
@@ -32,83 +32,98 @@ namespace Swagger.SwaggerToMarkdown
             {
                 var doc = new MarkdownDocument();
 
+                var query = httpContext.Request.Query;
+                query.TryGetValue("apiPath", out var apiPath);
+                query.TryGetValue("apiMethod", out var apiMethod);
+
                 var swagger = _swaggerProvider.GetSwagger(_options.DocumentName);
                 foreach (var path in swagger.Paths)
                 {
                     foreach (var operation in path.Value.Operations)
                     {
-                        doc.WriteH4("简要描述");
-                        doc.WriteUnorderedList(operation.Value.Summary);
-
-                        doc.WriteH4("请求URL");
-                        doc.WriteUnorderedList(path.Key);
-
-                        doc.WriteH4("请求方式");
-                        doc.WriteUnorderedList(operation.Key.ToString());
-
-                        doc.WriteH4("参数");
-                        doc.WriteTable(builder =>
+                        if (!string.IsNullOrEmpty(apiPath) && apiPath.ToString().Equals(path.Key, StringComparison.OrdinalIgnoreCase) && !string.IsNullOrEmpty(apiMethod) && apiMethod.ToString().Equals(operation.Key.ToString(), StringComparison.OrdinalIgnoreCase))
                         {
-                            if (operation.Value.Parameters.Count > 0)
-                            {
-                                builder.AddHeader("参数名", "必选", "类型", "说明");
+                            doc.WriteH4("简要描述");
+                            doc.WriteUnorderedList(operation.Value.Summary);
 
-                                foreach (var parameter in operation.Value.Parameters)
-                                {
-                                    builder.AddRow(parameter.Name.ToTitleCase(), parameter.Required ? "是" : "否", parameter.Schema.Type, parameter.Description);
-                                }
-                            }
-                            else
-                            {
-                                if (operation.Value.RequestBody != null)
-                                {
-                                    var openApiMediaType = operation.Value.RequestBody.Content["application/json"];
-                                    var reference = openApiMediaType.Schema.Reference.ReferenceV3;
+                            doc.WriteH4("请求URL");
+                            doc.WriteUnorderedList(path.Key);
 
-                                    var openApiSchema = GetSchema(swagger, reference);
-                                    if (openApiSchema.Properties.Count > 0)
+                            doc.WriteH4("请求方式");
+                            doc.WriteUnorderedList(operation.Key.ToString());
+
+                            doc.WriteH4("参数");
+                            doc.WriteTable(builder =>
+                            {
+                                if (operation.Value.Parameters.Count > 0)
+                                {
+                                    builder.AddHeader("参数名", "必选", "类型", "说明");
+
+                                    foreach (var parameter in operation.Value.Parameters)
                                     {
-                                        builder.AddHeader("参数名", "必选", "类型", "说明");
+                                        builder.AddRow(parameter.Name.ToTitleCase(), parameter.Required ? "是" : "否", parameter.Schema.Type, parameter.Description);
+                                    }
+                                }
+                                else
+                                {
+                                    if (operation.Value.RequestBody != null)
+                                    {
+                                        var openApiMediaType = operation.Value.RequestBody.Content["application/json"];
+                                        var reference = openApiMediaType.Schema.Reference.ReferenceV3;
 
-                                        foreach (var property in openApiSchema.Properties)
+                                        var openApiSchema = GetSchema(swagger, reference);
+                                        if (openApiSchema.Properties.Count > 0)
                                         {
-                                            builder.AddRow(property.Key.ToTitleCase(), "否", property.Value.Type, property.Value.Description);
+                                            builder.AddHeader("参数名", "必选", "类型", "说明");
+
+                                            foreach (var property in openApiSchema.Properties)
+                                            {
+                                                builder.AddRow(property.Key.ToTitleCase(), "否", property.Value.Type, property.Value.Description);
+                                            }
                                         }
                                     }
                                 }
-                            }
-                        });
+                            });
 
-                        doc.WriteH4("返回示例");
-                        var responseTypeJson = operation.Value.Extensions["ResponseType"].ToString();
-                        doc.WriteCode(responseTypeJson);
+                            doc.WriteH4("返回示例");
+                            var responseTypeJson = operation.Value.Extensions["ResponseType"].ToString();
+                            doc.WriteCode(responseTypeJson);
 
-                        doc.WriteH4("返回参数说明");
-                        doc.WriteTable(builder =>
-                        {
-                            if (operation.Value.Responses.TryGetValue("200", out var openApiResponse))
+                            doc.WriteH4("返回参数说明");
+                            doc.WriteTable(builder =>
                             {
-                                if (openApiResponse.Content.TryGetValue("application/json", out var openApiMediaType))
+                                if (operation.Value.Responses.TryGetValue("200", out var openApiResponse))
                                 {
-                                    var reference = openApiMediaType.Schema.Reference?.ReferenceV3 ?? openApiMediaType.Schema.Items?.Reference?.ReferenceV3;
-                                    var openApiSchema = GetSchema(swagger, reference);
-
-                                    if (openApiSchema != null && openApiSchema.Properties.Count > 0)
+                                    if (openApiResponse.Content.TryGetValue("application/json", out var openApiMediaType))
                                     {
-                                        builder.AddHeader("参数名", "类型", "说明");
+                                        var reference = openApiMediaType.Schema.Reference?.ReferenceV3 ?? openApiMediaType.Schema.Items?.Reference?.ReferenceV3;
+                                        var openApiSchema = GetSchema(swagger, reference);
 
-                                        var prefix = string.Empty;
-                                        AddProperties(builder, openApiSchema.Properties, ref prefix, swagger);
+                                        if (openApiSchema != null && openApiSchema.Properties.Count > 0)
+                                        {
+                                            builder.AddHeader("参数名", "类型", "说明");
+
+                                            var prefix = string.Empty;
+                                            AddProperties(builder, openApiSchema.Properties, ref prefix, swagger);
+                                        }
                                     }
                                 }
-                            }
-                        });
+                            });
+                        }
                     }
                 }
 
-                httpContext.Response.Headers.Add("Content-Disposition", "attachment; filename=swaggerToMarkdown.md; filename*=UTF-8''swaggerToMarkdown.md");
-                httpContext.Response.ContentType = "text/markdown";
-                await httpContext.Response.WriteAsync(doc.ToString());
+                if (string.IsNullOrEmpty(doc.ToString()))
+                {
+                    httpContext.Response.StatusCode = 400;
+                }
+                else
+                {
+                    httpContext.Response.Headers.Add("Content-Disposition", "attachment; filename=swaggerToMarkdown.md; filename*=UTF-8''swaggerToMarkdown.md");
+                    httpContext.Response.ContentType = "text/markdown";
+
+                    await httpContext.Response.WriteAsync(doc.ToString());
+                }
             }
             else
             {
